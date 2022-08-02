fe391b8a98adee257f8b677d6b9b9d842e6720a1
Nigel <hypej.10@gmail.com>
2022-08-02 14:22:06 +0200

    fix test

--- src/app/city-search/city-search.component.spec.ts
+++ src/app/city-search/city-search.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { WeatherService } from '../weather/weather.service';
 import { CitySearchComponent } from './city-search.component';
 
 describe('CitySearchComponent', () => {
   let component: CitySearchComponent;
   let fixture: ComponentFixture<CitySearchComponent>;
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>;
 
   beforeEach(async () => {
+    const weatherServiceSpy = jasmine.createSpyObj('WeatherService', [
+      'getCurrentWeather',
+    ]);
     await TestBed.configureTestingModule({
-      declarations: [ CitySearchComponent ]
+
+      declarations: [ CitySearchComponent ],
+      providers: [
+        {
+          provide: WeatherService,
+          useValue: weatherServiceSpy,
+        },
+      ],
     })
     .compileComponents();
 
